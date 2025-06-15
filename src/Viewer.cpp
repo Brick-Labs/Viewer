85850994b5cba0df06135499766cb0d49fb05816
Andreas Böhm <134922046+LordofGhost@users.noreply.github.com>
2025-06-15 21:18:07 +0200

    Create Vulkan window with GLFW

--- src/Viewer.cpp
+++ src/Viewer.cpp
@@ -1,9 +1,47 @@
 #include "Viewer.h"
 
-#include <iostream>
+#include <GLFW/glfw3.h>
 #include <Renderer.h>
 
+#include <iostream>
+
 int main() {
-    std::cout << "Viewer" << std::endl;
-    Renderer::init();
+    Viewer app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void Viewer::run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanUp();
+}
+
+void Viewer::initWindow() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Viewer", nullptr, nullptr);
+}
+
+void Viewer::initVulkan() {}
+void Viewer::mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+}
+void Viewer::cleanUp() {
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 }
--- src/Viewer.h
+++ src/Viewer.h
@@ -1,12 +1,19 @@
 #ifndef VIEWER_H
 #define VIEWER_H
-
-
+#include "GLFW/glfw3.h"
 
 class Viewer {
+   public:
+    void run();
 
+   private:
+    GLFWwindow* window;
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+    void initWindow();
+    void initVulkan();
+    void mainLoop();
+    void cleanUp();
 };
 
-
-
-#endif //VIEWER_H
+#endif  // VIEWER_H
